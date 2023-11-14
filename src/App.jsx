f9d0d25859e83c54b0e9e83fd8f095d2189854f3
RosaMonro <rosamaria.monro@gmail.com>
2023-11-14 11:44:33 +0100

    Install react reuter dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Styleguide from "./styleguide/Styleguide";
 
-
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/Styleguide" element={<Styleguide />} />
+      </Routes>
+    </>
+  );
+}
+export default App;
---
+++ src/styleguide/Styleguide.jsx
@@ -0,0 +1,7 @@
+export default function Styleguide() {
+  return (
+    <>
+      <h1>Hola soy la guia</h1>
+    </>
+  );
+}
