249051f8e9b87e8e34341c1252295612a4cae26f
RosaMonro <rosamaria.monro@gmail.com>
2023-11-22 13:25:41 +0100

    margin sections

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -6,13 +6,33 @@ export default function Home() {
       <header>
         {/* <Nav></Nav> */}
         <div className="titles">
-          <h2 className="title h1 font-montse">
+          <h2 className="m-none h1 font-montse">
             ROSA <br />
             MONTERO
           </h2>
-          <h1 className="title font-gruppo">UX DESIGNER</h1>
+          <h1 className="m-none subtitle font-gruppo">UX DESIGNER</h1>
         </div>
       </header>
+
+      <section className="m-sections">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit id eu lacinia,
+          vehicula mauris dapibus vitae nullam blandit consequat himenaeos ante,
+          per ac imperdiet metus hendrerit iaculis turpis velit pellentesque.
+          Morbi eros aliquet est sollicitudin facilisi lectus convallis vel
+          vehicula ac molestie, mi mus fermentum fames risus scelerisque augue
+          odio rhoncus posuere. Habitant tempor tellus convallis venenatis nisl
+          inceptos scelerisque proin luctus congue rhoncus, rutrum per dui quam
+          ac faucibus metus facilisis curabitur aliquam, libero montes interdum
+          feugiat phasellus vitae viverra orci placerat aenean. Vel mollis morbi
+          sociosqu ornare feugiat montes neque mi interdum diam gravida et
+          habitasse, sollicitudin viverra pulvinar lacinia convallis vulputate
+          arcu lobortis tempor habitant tellus enim. Laoreet ligula nascetur
+          pulvinar ullamcorper pellentesque fusce id facilisis, vulputate odio
+          platea fermentum libero dui curabitur vivamus, natoque dictum sapien
+          luctus arc
+        </p>
+      </section>
     </>
   );
 }
