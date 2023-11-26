5ca958ece703039f5c6f1c7a2218ba7e65639911
RosaMonro <rosamaria.monro@gmail.com>
2023-11-26 13:51:47 +0100

    Contenido "sobre mí"

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -45,18 +45,84 @@ export default function Home() {
         <div className="dropdowns">
           <Dropdown
             title="UX/UI"
-            abstractDescription="orem ipsum dolor sit amet consectetur adipiscing elit habitasse, at t"
-            description="Lorem ipsum dolor sit amet consectetur adipiscing elit habitasse, at tellus curabitur curae aliquam morbi tempor class, id aptent facilisi per ullamcorper egestas nisi. Etiam molestie velit dictumst fermentum porta habitant sapien himenaeos proin gravida curae fringilla pellentesque inceptos primis, eleifend ultrices aptent facilisi varius nullam curabitur bibendum ac tristique litora faucibus senectus. Commodo dis molestie consequat laoreet suspendisse arcu enim, potenti nibh pretium elementum vestibulum donec porta porttitor, iaculis scelerisque ad dignissim fusce maecenas."
+            abstractDescription="Especialmente interesada en lo que la neurociencia y el neurodiseño pueden hacer por la creación de productos digitales centrados en el usuario que satisfagan las necesidades de usuarios y clientes."
+            description={
+              <>
+                <p>
+                  Especialmente interesada en lo que la neurociencia y el
+                  neurodiseño pueden hacer por la creación de productos
+                  digitales centrados en el usuario que satisfagan las
+                  necesidades de usuarios y clientes.
+                </p>
+                <p>
+                  Trato de mantenerme activa con proyectos personales de
+                  rediseño de interfaces con Figma y procuro aplicar mis
+                  conocimientos previos en la investigación de usuario, diseño
+                  de interacción, pruebas de usuario, etc.
+                </p>
+                <p>
+                  Tanto la psicología como el diseño y desarrollo de interfaces
+                  requieren habilidades técnicas y analíticas, la capacidad de
+                  identificar patrones de conducta del usuario y una atención
+                  meticulosa a los detalles.
+                </p>
+              </>
+            }
           ></Dropdown>
           <Dropdown
             title="Frontend-UI"
-            abstractDescription="orem ipsum dolor sit amet consectetur adipiscing elit habitasse, at t"
-            description="Lorem ipsum dolor sit amet consectetur adipiscing elit habitasse, at tellus curabitur curae aliquam morbi tempor class, id aptent facilisi per ullamcorper egestas nisi. Etiam molestie velit dictumst fermentum porta habitant sapien himenaeos proin gravida curae fringilla pellentesque inceptos primis, eleifend ultrices aptent facilisi varius nullam curabitur bibendum ac tristique litora faucibus senectus. Commodo dis molestie consequat laoreet suspendisse arcu enim, potenti nibh pretium elementum vestibulum donec porta porttitor, iaculis scelerisque ad dignissim fusce maecenas."
+            abstractDescription="Siempre en constante aprendizaje, tanto desarrollo frontend como en backend, bajo la premisa de crear código limpio, escalable  y accesible con el uso de buenas prácticas. "
+            description={
+              <>
+                <p>
+                  Siempre en constante aprendizaje, tanto desarrollo frontend
+                  como en backend, bajo la premisa de crear código limpio,
+                  escalable y accesible con el uso de buenas prácticas.
+                </p>
+                <p>
+                  Inicialmente me formé en HTML, CSS y JavaScript en el posgrado
+                  de UX/UI, desarrollo frontend y marketing digital en la
+                  Escuela Superior de Arte y Tecnología de Valencia. Allí además
+                  aprendí el uso de Sass así como la aplicación de la
+                  metodología BEM.
+                </p>
+                <p>
+                  Actualmente he comenzado mi andadura a través del mundo
+                  backend. Mi objetivo es lograr una comprensión integral del
+                  desarrollo para poder aportar soluciones completas y eficaces
+                  al equipo.
+                </p>
+              </>
+            }
           ></Dropdown>
           <Dropdown
             title="Neurociencia"
-            abstractDescription="orem ipsum dolor sit amet consectetur adipiscing elit habitasse, at t"
-            description="Lorem ipsum dolor sit amet consectetur adipiscing elit habitasse, at tellus curabitur curae aliquam morbi tempor class, id aptent facilisi per ullamcorper egestas nisi. Etiam molestie velit dictumst fermentum porta habitant sapien himenaeos proin gravida curae fringilla pellentesque inceptos primis, eleifend ultrices aptent facilisi varius nullam curabitur bibendum ac tristique litora faucibus senectus. Commodo dis molestie consequat laoreet suspendisse arcu enim, potenti nibh pretium elementum vestibulum donec porta porttitor, iaculis scelerisque ad dignissim fusce maecenas."
+            abstractDescription="Con más de 10 años de experiencia en neurorrehabilitación, la neurociencia ya forma parte
+            intrínseca de mí. Ahora busco integrar los principios
+            neurocientíficos al diseño y al desarrollo web."
+            description={
+              <>
+                <p>
+                  Con más de 10 años de experiencia en neurorrehabilitación, la
+                  neurociencia ya forma parte intrínseca de mí. Ahora busco
+                  integrar los principios neurocientíficos al diseño y al
+                  desarrollo web.
+                </p>
+                <p>
+                  La neuropsicología explica cómo reaccionamos ante diferentes
+                  estímulos, generando emociones y configurando la conducta.
+                  Entendiendo que nuestro comportamiento está impulsado por
+                  procesos neuronales, apreciamos la importancia de éstos en el
+                  diseño.
+                </p>
+                <p>
+                  Tengo amplia experiencia en la administración e interpretación
+                  de valoraciones cognitivas, análisis de conducta y creación de
+                  planes de tratamiento coordinando y colaborando con equipos
+                  intradisciplinares.
+                </p>
+              </>
+            }
           ></Dropdown>
         </div>
       </section>
